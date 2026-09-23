baf099951bfc83e70939a54bcc33bf7fbd3fa438
Vimalesh <vimal7123@gmail.com>
2026-09-23 17:56:36 +0530

    feed api created

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { userAuth } from "../middleware/auth.js";
 import { ConnectionRequest } from "../models/connectionRequest.js";
+import { userModel } from "../models/user.js";
 
 export const userRoute = express.Router();
 
@@ -64,10 +65,7 @@ userRoute.get("/user/connections", userAuth, async (req, res) => {
         }
 
         const data = connectionRequests.map((row) => {
-            if (
-                row.toUserId._id.toString() ===
-                loggedInUser._id.toString()
-            ) {
+            if (row.toUserId._id.toString() === loggedInUser._id.toString()) {
                 return row.fromUserId;
             }
 
@@ -77,6 +75,63 @@ userRoute.get("/user/connections", userAuth, async (req, res) => {
         res.json({
             message: "Accepted connections list fetched successfully!",
             data,
+        });
+    } catch (error) {
+        res.status(400).send("Error : " + error.message);
+    }
+});
+
+userRoute.get("/feed", userAuth, async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = Math.min(parseInt(req.query.limit) || 10, 50);
+        const skip = (page - 1) * limit;
+
+        const USER_SAFE_DATA = [
+            "firstName",
+            "lastName",
+            "age",
+            "about",
+            "photoUrl",
+        ];
+
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {
+                    fromUserId: loggedInUser._id,
+                },
+                {
+                    toUserId: loggedInUser._id,
+                },
+            ],
+        }).select(["fromUserId", "toUserId"]);
+
+        const hideUserFromFeed = new Set();
+
+        connectionRequests.forEach((req) => {
+            (hideUserFromFeed.add(req.fromUserId.toString()),
+                hideUserFromFeed.add(req.toUserId.toString()));
+        });
+        const user = await userModel
+            .find({
+                $and: [
+                    {
+                        _id: { $nin: Array.from(hideUserFromFeed) },
+                    },
+                    {
+                        _id: { $ne: loggedInUser._id },
+                    },
+                ],
+            })
+            .select(USER_SAFE_DATA)
+            .skip(skip)
+            .limit(limit);
+
+        res.json({
+            message: "Feed user fetched successfully!",
+            data: user,
         });
     } catch (error) {
         res.status(400).send("Error : " + error.message);
